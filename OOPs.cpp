596c2cf5eb21ad234da90a77e02e3d937fb238a0
sanyam singhai <sanyamsinghai041@gmail.com>
2024-03-06 08:23:37 +0530

    first commit added basic code

---
+++ OOPs.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+    class hero{
+
+        public:
+        int health;
+        string name;
+    };
+
+int main(){
+    hero h1;
+
+    return 0;
+}
