c8a64711e3810927f84accdbf96e047ec7d8f85f
sanyam singhai <sanyamsinghai041@gmail.com>
2024-03-10 22:05:11 +0530

    rename file

---
+++ OOPsEncapsulation.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+
+//encapsulation 
+
+    class hero{
+        private:
+        int health;
+        int weight;
+
+        public:
+        int gethealth(){
+            return this->health;
+        }
+
+        void sethealth(int h){
+            this->health=h;
+        }
+    };
+
+int main(){
+    hero h1;
+
+    h1.sethealth(75);
+    cout<<"health is : "<<h1.gethealth()<<endl;
+    
+    return 0;
+}
