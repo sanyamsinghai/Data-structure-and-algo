8fa1317255db53cb2d0dbc6e30c01f28f51749fb
sanyam singhai <sanyamsinghai041@gmail.com>
2024-03-26 13:32:50 +0530

    swap array and check string is palindrom or not

--- Recursion2.cpp
+++ Recursion2.cpp
@@ -1,30 +1,52 @@
 #include<iostream>
+#include<string>
 using namespace std;
 
-void func1(int i,int arr[],int n){
-    if(i>=n/2)return;
+//                                                                SWAP ARRAY 
+// void func1(int i,int arr[],int n){
+//     if(i>=n/2)return;
 
-    swap(arr[i],arr[n-i-1]);
-    func1(i+1,arr,n);  
+//     swap(arr[i],arr[n-i-1]);
+//     func1(i+1,arr,n);  
+// }
+
+// int main(){
+//     int n;
+//     cout<<"enter size of array : ";
+//     cin>>n;
+
+//     int *arr=new int[n];
+
+//     for(int i=0 ; i<n ; i++){
+//         cin>>arr[i];
+//     }
+
+//     func1(0,arr,n);
+//     for(int i=0 ; i<n ; i++){
+//         cout<<arr[i]<<" ";
+//     }
+//     cout<<endl;
+
+//     delete[] arr;
+//     return 0;
+// }
+
+//                                            CHECK STRING IS PALINDROM OR NOT
+
+bool fuc1(int i,string &s){
+    if(i>=s.size()/2)return true;
+
+    if(s[i]!=s[s.size()-i-1]){
+        return false;
+    }
+
+    return fuc1(i+1,s);
 }
 
 int main(){
-    int n;
-    cout<<"enter size of array : ";
-    cin>>n;
+    string s="madsm";
 
-    int *arr=new int[n];
+    cout<<fuc1(0,s);
 
-    for(int i=0 ; i<n ; i++){
-        cin>>arr[i];
-    }
-
-    func1(0,arr,n);
-    for(int i=0 ; i<n ; i++){
-        cout<<arr[i]<<" ";
-    }
-    cout<<endl;
-
-    delete[] arr;
     return 0;
 }
